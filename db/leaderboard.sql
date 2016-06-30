1a1f95ef5fcaf0cfa7070a155e2e0688644bcd5a
benjdj6 <benjdj6@vt.edu>
2016-06-30 13:37:52 -0700

    Added leaderboard, and filter options stub, added DB, React now using data from server

--- db/leaderboard.sql
+++ db/leaderboard.sql
@@ -1,7 +1,3 @@
-#Author Ben Johnston, github: benjdj6
-#Original: 06/29/16
-#Current: 06/29/16
-
 CREATE TABLE leaderboard (
 	username	text PRIMARY KEY,
 	points		int,
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const Hapi = require('hapi');
 const pg = require('pg');
 const Path = require('path');
 
+const conString = "postgres://testuser:password@localhost:5432/leaderboard";
+
 const server = new Hapi.Server({
     connections: {
         routes: {
@@ -12,6 +14,23 @@ const server = new Hapi.Server({
     }
 });
 server.connection({port: 3000});
+
+var tableModel = {
+	cols: ["Name", "Age"],
+	rows: [{
+		"Name": "Chase",
+		"Age": "27"
+		},
+		{	
+		"Name": "Ben",
+		"Age": "23"
+		}]
+}
+
+var response = {
+	cols: ["username", "points", "state", "university"],
+	rows: []		
+}
 
 server.register(require('inert'), (err) => {
 
@@ -26,6 +45,50 @@ server.register(require('inert'), (err) => {
             reply.file('index.html');
         }
     });
+
+    server.route({
+    	method: 'GET',
+    	path: '/leaderboard',
+    	handler: function (request, reply) {
+    		pg.connect(conString, function(err, client, done) {
+
+				var handleError = function(err) {
+		    	// no error occurred, continue with the request
+		    		if(!err) return false;
+
+		    		// An error occurred, remove the client from the connection pool.
+		    		// A truthy value passed to done will remove the connection from the pool
+		    		// instead of simply returning it to be reused.
+		    		// In this case, if we have successfully received a client (truthy)
+		    		// then it will be removed from the pool.
+		    		if(client){
+		    		  done(client);
+		    		}
+		    		console.log(err);
+		   			reply('error occurred');
+		    		return true;
+		  		};
+
+		    	// handle an error from the connection
+		    	if(handleError(err)) {
+		    		return;
+		    	}
+
+		      	// get all of the tasks in the table
+		      	client.query('SELECT * FROM leaderboard ORDER BY points DESC;', function(err, result) {
+
+		        	// handle an error from the query
+		        	if(handleError(err)) return;
+
+		        	// return the client to the connection pool for other requests to reuse
+		        	response.rows = result.rows;
+		        	console.log(response);
+		        	reply(response);
+		    	});
+			});
+    		//reply(tableModel);
+    	}
+    })
 
     server.start((err) => {
 
